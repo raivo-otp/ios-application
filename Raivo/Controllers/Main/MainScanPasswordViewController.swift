e28b56d866bf0ca3db6796009e0f343631074474
Tijme Gommers <t.gommers@outlook.com>
2021-01-21 14:48:17 +0100

    Navigate to new view controller instead of exporting by e-mail (feature not yet finished). Related to issue #24.

--- Raivo/Controllers/Main/MainScanPasswordViewController.swift
+++ Raivo/Controllers/Main/MainScanPasswordViewController.swift
@@ -11,7 +11,6 @@
 // 
 
 import UIKit
-import UIKit
 import AVFoundation
 import OneTimePassword
 
--- Raivo/Forms/MiscellaneousForm.swift
+++ Raivo/Forms/MiscellaneousForm.swift
@@ -211,40 +211,7 @@ class MiscellaneousForm {
                 cell.textLabel?.textAlignment = .left
                 cell.imageView?.image = UIImage(named: "form-zip")
             }).onCellSelection({ cell, row in
-                let barButtonItem = controller.displayNavBarActivity()
-                
-                BannerHelper.shared.done("Hold tight", "Generation takes a few seconds", wrapper: controller.view)
-                
-                // Run in the background and fake a delay so the user can read the banner
-                DispatchQueue.global(qos: .background).asyncAfter(deadline: .now() + 1, execute: {
-                    let dataExport = DataExportFeature()
-
-                    let status = autoreleasepool { () -> DataExportFeature.Result in
-                        let password = StorageHelper.shared.getEncryptionPassword()
-                        return dataExport.generateArchive(protectedWith: password!)
-                    }
-                    
-                    guard case let DataExportFeature.Result.success(archive) = status else {
-                        log.error("Archive generation failed!")
-                        return
-                    }
-                    
-                    let dataExportMail = ComposeMailFeature(.dataExport)
-                    
-                    DispatchQueue.main.async {
-                        if dataExportMail.canSendMail() {
-                            dataExportMail.addAttachment(archive, "application/zip", "raivo-otp-export.zip")
-                            dataExportMail.send(popupFrom: controller) {
-                                controller.dismissNavBarActivity(barButtonItem)
-                            }
-                        } else {
-                            let activity = UIActivityViewController(activityItems: [archive], applicationActivities: nil)
-                            controller.present(activity, animated: true, completion: {
-                                controller.dismissNavBarActivity(barButtonItem)
-                            })
-                        }
-                    }
-                })
+                controller.performSegue(withIdentifier: "MainDataExportSegue", sender: self)
             })
     }
     
